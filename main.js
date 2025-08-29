70764e01cd7b25240532aea72c96a421392bd291
Pgnop <penguingnop@gmail.com>
2025-08-29 22:24:47 +0800

    a

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import prompt from 'prompt-sync';
 
 process.stdout.write('\x1Bc'); // Clear terminal
+
+// Get user input
 const input = prompt()('$ ');
+
+// Echo command. Check if the input starts with 'echo'
 if (input.startsWith('echo ')) {
-    console.log(input.slice(5));
-} else {
-    console.log(input);
+    console.log(input.slice(5)); // Echo the input after 5 letters
 }
