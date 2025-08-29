c221a0759d51108ae1df5a63d3c871ce92352ad2
Pgnop <penguingnop@gmail.com>
2025-08-29 22:41:05 +0800

    added cd

--- main.js
+++ main.js
@@ -6,19 +6,24 @@ process.stdout.write('\x1Bc'); // Clear terminal
 while(true) {
     // Get user input
     const input = prompt()('$ ');
-    
+
     // Echo command. Check if the input starts with 'echo'
     if (input.startsWith('echo ')) {
         console.log(input.slice(5)); // Echo the input after 5 letters
     }
-    
+
     // Say command. Check if the input starts with 'say'
     if (input.startsWith('say ')) {
         say.speak(input.slice(4)); // Speak the input after 4 letters
     }
-    
+
     // Pwd command. Check if the input is 'pwd'
     if (input === 'pwd'.toLowerCase()) {
         console.log(process.cwd()); // Print the current working directory
+    }
+
+    // Cd command. Check if the input starts with 'cd'
+    if (input.startsWith('cd ')) {
+        process.chdir(input.slice(3)); // Change the current working directory
     }
 }
